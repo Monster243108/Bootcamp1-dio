4bbda6c73624b5ccdb9c80bc935a802e975cbe33
Marcio Fernando Evangelista <evangelistanovo@gmail.com>
2022-06-30 22:50:01 -0300

    Update scripts.js

--- desafio-contador/assets/js/scripts.js
+++ desafio-contador/assets/js/scripts.js
@@ -1,13 +1,19 @@
-var currentNumberWrapper = document.getElementById('currentNumber');
-var currentNumber = 0;
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
 
 function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    count++;
+    CURRENT_NUMBER.innerHTML = count;
 }
 
 function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    count--;
+    CURRENT_NUMBER.innerHTML = count;
+    
+}
+
+function test(){
+    kdowkdpo;
 }
 
